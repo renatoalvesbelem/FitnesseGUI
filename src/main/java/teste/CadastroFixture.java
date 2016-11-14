295cadc12f925adce4698363d4ad831f7ea6e73c
José Renato da Silva Alves <jose.renato@softplan.com.br>
2016-11-14 09:48:26 -0200

    Alterado componente Jtable por JArea, para descrição da nova fixture a ser cadastrada/ Alterada

--- src/main/java/teste/CadastroFixture.java
+++ src/main/java/teste/CadastroFixture.java
@@ -85,26 +85,13 @@ public class CadastroFixture {
 
                 gui.add(panelAll, BorderLayout.NORTH);
 
-
                 setPanelParametros();
 
-                String[] header = {"Name", "Value"};
-                String[] a = new String[0];
-                String[] names = System.getProperties().
-                        stringPropertyNames().toArray(a);
-                String[][] data = new String[names.length][2];
-                for (int ii = 0; ii < names.length; ii++) {
-                    data[ii][0] = names[ii];
-                    data[ii][1] = System.getProperty(names[ii]);
-                }
-                DefaultTableModel model = new DefaultTableModel(data, header);
-                JTable table = new JTable(model);
-                try {
-                    // 1.6+
-                    table.setAutoCreateRowSorter(true);
-                } catch (Exception continuewithNoSort) {
-                }
-                JScrollPane tableScroll = new JScrollPane(table);
+
+                JTextArea area = new JTextArea();
+                area.setLineWrap(true);
+                area.setSize(600,500);
+                JScrollPane tableScroll = new JScrollPane(area);
                 Dimension tablePreferred = tableScroll.getPreferredSize();
                 tableScroll.setPreferredSize(new Dimension(tablePreferred.width, tablePreferred.height / 3));
 
@@ -120,8 +107,8 @@ public class CadastroFixture {
                 JLabel imageLabel = new JLabel(ii);
                 imagePanel.add(imageLabel, null);
 
-                JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, tableScroll, new JScrollPane(imagePanel));
-                gui.add(splitPane, BorderLayout.CENTER);
+                //   JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, tableScroll, new JScrollPane(imagePanel));
+                gui.add(tableScroll, BorderLayout.EAST);
 
                 frame.setContentPane(gui);
 
